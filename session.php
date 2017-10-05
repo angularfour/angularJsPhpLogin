e8e0866fbb1d415ef72cde8e6bfc87e63a705e64
Danilo Batista de Queiroz <angularfour@gmail.com>
2017-10-05 01:26:32 -0300

    login working

--- session.php
+++ session.php
@@ -1,17 +1,17 @@
 <?php
   // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-  $connection = mysql_connect("localhost", "root", "");
+  $connection = mysqli_connect("localhost", "usercom", "123");
   // Selecting Database
-  $db = mysql_select_db("company", $connection);
+  $db = $connection->select_db("employee");
   session_start();// Starting Session
   // Storing Session
   $user_check=$_SESSION['login_user'];
   // SQL Query To Fetch Complete Information Of User
-  $ses_sql=mysql_query("select username from login where username='$user_check'", $connection);
-  $row = mysql_fetch_assoc($ses_sql);
+  $ses_sql=$connection->query("select username from login where username='$user_check'");
+  $row = mysqli_fetch_assoc($ses_sql);
   $login_session =$row['username'];
   if(!isset($login_session)){
-    mysql_close($connection); // Closing Connection
+    mysqli_close($connection); // Closing Connection
     header('Location: index.php'); // Redirecting To Home Page
   }
 ?>
